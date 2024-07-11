c3e37daa099fe33da0a6a6ed158964258c9d5aac
Ank-pro <ank.pro0115@gmail.com>
2024-07-11 18:29:23 +0530

    prod-fix

--- routes/Product.js
+++ routes/Product.js
@@ -8,7 +8,7 @@ const app = express();
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, path.join(__dirname, '..', 'public', 'uploads'));
+        cb(null, path.join(__dirname, '..', 'public', 'uploads' ));
     },
     filename: function (req, file, cb) {
         cb(null, file.originalname);
