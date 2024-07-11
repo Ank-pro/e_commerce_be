d9d0d27d4caf8deb35a9a57fae1fb111c21ca518
Ank-pro <ank.pro0115@gmail.com>
2024-07-11 19:12:34 +0530

    mStorage

--- routes/Product.js
+++ routes/Product.js
@@ -15,7 +15,7 @@ const storage = multer.diskStorage({
     }
 });
 
-const upload = multer({ storage: storage });
+const upload = multer({ storage: multer.memoryStorage() });
 
 
 route.get('/', (req, res) => {
